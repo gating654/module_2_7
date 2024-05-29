26534f7f472440b3051b3f0d9ffb5a8714088f81
gating654 <168896707+gating654@users.noreply.github.com>
2024-05-29 13:49:39 +0700

    Update module_2_7.py

--- module_2_7.py
+++ module_2_7.py
@@ -1,10 +1,12 @@
 def get_multiplied_digits(number):
     str_number = str(number)
-    first = int(str_number[1:])
-    if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
-    else:
-        return first
+
+    if len(str_number) < 2:
+        return int(str_number[:1])
+
+    first = int(str_number[:1])
+    return first * get_multiplied_digits(int(str_number[1:]))
+
 
 
 result = get_multiplied_digits(40203)
